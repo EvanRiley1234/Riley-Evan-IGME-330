d5955e467dc9a46912f622a675ee6b870ff41d9c
EvanRiley1234 <54547434+EvanRiley1234@users.noreply.github.com>
2023-01-23 17:24:38 -0500

    PE-01

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+        
+const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+//console.log(words1[0]);
+generate();
+document.querySelector("#myButton").onclick = generate;
+//document.querySelector("#myButton").addEventListener("click", generate);
+
+function randomElement(array){
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+function generate(){
+    //const w1 = words1[Math.floor(Math.random() * words1.length)];
+    //const w2 = words2[Math.floor(Math.random() * words2.length)];
+    //const w3 = words3[Math.floor(Math.random() * words3.length)];
+
+    const str = randomElement(words1) + " " + randomElement(words2) + " " + randomElement(words3);
+    //return `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}`;
+    document.querySelector("#output").innerHTML = str;
+}
